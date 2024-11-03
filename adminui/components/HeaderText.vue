03a082a72640b5d3788a78f1a9e5f4a96d93d962
Udee101 <franklynudeorah@gmail.com>
2024-11-03 17:18:48 +0100

    create new header component

---
+++ adminui/components/HeaderText.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-container">
+    <h1>Payoor <span>{{ pageText }}</span></h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      pageText: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.header-container h1 {
+  color: $primary-color;
+}
+span {
+  color: $white;
+  font-size: 1.3rem;
+  font-weight: 500;
+}
+</style>
