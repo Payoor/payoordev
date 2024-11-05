46ced041948a76998e15b4f445bbe747fc48545e
Udee101 <franklynudeorah@gmail.com>
2024-11-05 20:09:33 +0100

    add money format helper

---
+++ adminui/helpers.js
@@ -0,0 +1,9 @@
+export const formatAmount = (amount) => {
+  const formatter = new Intl.NumberFormat("en-NG", {
+    style: "currency",
+    currency: "NGN",
+    minimumFractionDigits: 0,
+  });
+
+  return formatter.format(amount);
+}
