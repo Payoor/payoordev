80f76f8e9f4605a811c2c502ebb1e60aed89de05
emmanuel <nerdyemmanuel@gmail.com>
2024-11-06 04:09:00 +0100

    add cors allowance for dev admin and deploy admin ui updates to dev

--- nodeserver/server/corsOriginArray.js
+++ nodeserver/server/corsOriginArray.js
@@ -1,7 +1,8 @@
 const corsOriginArray = [
     'https://chat.payoor.shop',
     'https://admin.payoor.shop',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://admin.development.payoor.store'
 ]
 
 export default corsOriginArray;
