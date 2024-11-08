cfe1388c5c1990d5139c0a36fe2dfecd45e89f39
Udee101 <franklynudeorah@gmail.com>
2024-11-08 07:09:53 +0100

    add admin management api requests

--- adminui/api/index.js
+++ adminui/api/index.js
@@ -1,8 +1,18 @@
 import { getDefaultHeader, getFileHeader, serverUrl } from "./config";
 import $http from './http'
 
+
+// Admin Management
 export const adminLogin = (data) => $http.post(`${serverUrl}/admin/login`, data);
 
+export const getAllAdmins = () => $http.get(`${serverUrl}/admins`, getDefaultHeader());
+
+export const createAdmin = (data) => $http.post(`${serverUrl}/admin/create`, data, getDefaultHeader());
+
+export const removeAdmin = (adminId) => $http.delete(`${serverUrl}/admin/${adminId}`, getDefaultHeader());
+
+
+// Product Manangement
 export const uploadExcelSheet = (data) => $http.post(`${serverUrl}/admin/upload/products/excel`, data, getFileHeader());
 
 export const getAllProducts = () => $http.get(`${serverUrl}/admin/get/products`, getDefaultHeader());
