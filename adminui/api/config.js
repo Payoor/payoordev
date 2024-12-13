7e4444505840fc1870876a6d5187edcb4e8090bf
emmanuel <nerdyemmanuel@gmail.com>
2024-12-14 00:18:20 +0100

    add view cart, view orders

--- adminui/api/config.js
+++ adminui/api/config.js
@@ -15,4 +15,4 @@ export const getFileHeader = () => {
   }
 };
 
-export const serverUrl = 'http://localhost:3030'//'https://server.development.payoor.store';
+export const serverUrl = 'https://server.development.payoor.store';
