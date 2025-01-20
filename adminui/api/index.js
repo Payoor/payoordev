27fe0710c8268c39db364be83d791ac27becc402
Udee101 <franklynudeorah@gmail.com>
2025-01-20 23:13:29 +0100

    update remove image endpoint definition

--- adminui/api/index.js
+++ adminui/api/index.js
@@ -35,7 +35,7 @@ export const updateProductDetails = (productId, data) => $http.patch(`${serverUr
 
 export const uploadProductImage = (productId, data) => $http.post(`${serverUrl}/admin/upload/product/image?id=${productId}`, data, getFileHeader());
 
-export const removeProductImage = (imageId) => $http.delete(`${serverUrl}/admin/product/image?id=${imageId}`, getFileHeader());
+export const removeProductImage = ({ imageId, isVariant }) => $http.delete(`${serverUrl}/admin/product/image?id=${imageId}&isVariant=${isVariant}`, getFileHeader());
 
 export const removeProduct = (productId) => $http.delete(`${serverUrl}/admin/delete/product?id=${productId}`, getDefaultHeader());
 
